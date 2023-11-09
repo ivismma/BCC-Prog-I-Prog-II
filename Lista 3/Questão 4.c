24d17aa344b98b2cf4459d62d7c0cb5f2f94c99e
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2023-11-09 20:53:32 -0300

    Implementação da Q4

--- Lista 3/Questão 4.c
+++ Lista 3/Questão 4.c
@@ -1,16 +1,16 @@
 /*
  Implemente um programa que cumpra os seguintes requisitos:
-- Crie o tipo de dado Polinomio, fixado em grau 2 (isto é, todos os polinômios serão de 2o grau),
-e use um vetor de tamanho 3 para guardar os coeficientes (o quadrático, o linear e o
+- Crie o tipo de dado Polinomio, fixado em grau 2 (isto Ã©, todos os polinÃ´mios serÃ£o de 2o grau),
+e use um vetor de tamanho 3 para guardar os coeficientes (o quadrÃ¡tico, o linear e o
 independente);
-- Leia do teclado os coeficientes de dois polinômios, p1 e p2, desse tipo.
-- Imprima “Verdadeiro” se, para um inteiro N = 2, é o caso que p1 = N x p2 (ou vice-versa), e
-“Falso” caso contrário. Por exemplo:
+- Leia do teclado os coeficientes de dois polinÃ´mios, p1 e p2, desse tipo.
+- Imprima â€œVerdadeiroâ€ se, para um inteiro N >= 2, Ã© o caso que p1 = N x p2 (ou vice-versa), e
+â€œFalsoâ€ caso contrÃ¡rio. Por exemplo:
 Entrada:
 p1 = {9, 12, 21}
 p2 = {3, 4, 7}
-Saída:
-“Verdadeiro” pois 9x² + 12x + 21 == 3 * (3x² + 4x + 7), para N = 3.
+SaÃ­da:
+â€œVerdadeiroâ€ pois 9xÂ² + 12x + 21 == 3 * (3xÂ² + 4x + 7), para N = 3.
 */
 
 #include <stdio.h>
@@ -23,10 +23,10 @@ typedef struct{
 } Polinomio;
 
 
-// Retorna FALSE caso não exista ou o inteiro N encontrado caso exista.
+// Retorna FALSE caso nÃ£o exista ou o inteiro N encontrado caso exista.
 int verifica_p1p2(Polinomio p1, Polinomio p2){
 	int N = 2;
-	while(N < TAM){ // Exercício não especifica o limite de N, então será definido em 1000 (para não haver loop infinito).
+	while(N < TAM){ // ExercÃ­cio nÃ£o especifica o limite de N, entÃ£o serÃ¡ definido em 1000 (para nÃ£o haver loop infinito).
 		if( (p1.coef[0] + p1.coef[1] + p1.coef[2]) == (N * (p2.coef[0] + p2.coef[1] + p2.coef[2])) )
 			return N;
 		++N;
