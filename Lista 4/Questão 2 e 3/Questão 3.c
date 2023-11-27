d017d717a0b06ea080bd848f2f383e2e8ed1cee0
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2023-11-27 00:15:19 -0300

    Update Questão 3.c
    Correção de comentário //

--- Lista 4/Questão 2 e 3/Questão 3.c
+++ Lista 4/Questão 2 e 3/Questão 3.c
@@ -1,17 +1,17 @@
 /*
-Quest„o 3) Escreva um programa que receba o nome de um dos arquivos gerados no exercÌcio
-anterior (os quais agora j· armazenam 10 n˙meros em ponto flutuante). O programa dever· ent„o ler
-os valores desse arquivo e imprimir na tela o maior n˙mero encontrado.
+Quest√£o 3) Escreva um programa que receba o nome de um dos arquivos gerados no exerc√≠cio
+anterior (os quais agora j√° armazenam 10 n√∫meros em ponto flutuante). O programa dever√° ent√£o ler
+os valores desse arquivo e imprimir na tela o maior n√∫mero encontrado.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TAM 30 // Tamanho m·ximo do nome do arquivo: 26 caracteres
+#define TAM 30 // Tamanho m√°ximo do nome do arquivo: 26 caracteres
 
-// Para leitura de dados, vou utilizar o arquivo bin·rio (mais adequado).
+// Para leitura de dados, vou utilizar o arquivo bin√°rio (mais adequado).
 
-// Retorna o maior n˙mero de um vetor float.
+// Retorna o maior n√∫mero de um vetor float.
 float vetMaior(float v[]){
 	float maior = v[0];
 	for(int i = 0;i < 10; ++i) if(v[i] > maior) maior = v[i];
@@ -23,16 +23,16 @@ int main(){
 	FILE *arquivo;
 	float n[10];
 	
-	// Nome do arquivo de texto:
-	printf("Digite o nome do arquivo bin·rio que foi digitado na Q2: "); 
+	// Nome do arquivo bin√°rio que foi digitado na Q2 (sem incluir a extens√£o .dat):
+	printf("Digite o nome do arquivo bin√°rio que foi digitado na Q2: "); 
 	scanf("%[^\n]", str); strcat(str, ".dat"); getchar();
 	if(!(arquivo = fopen(str, "rb"))){
 		printf("Erro na abertura do arquivo - Nome invalido ou acesso negado.");
 		return -1;
 	}
-	// Leitura dos dados bin·rios
+	// Leitura dos dados bin√°rios
 	fread(n, sizeof(float), 10, arquivo);
-	// Impress„o do maior n˙mero: 
+	// Impress√£o do maior n√∫mero: 
 	float maior = vetMaior(n);
 	printf("Maior numero contido no arquivo \"%s\": %f", str, maior);
 	
