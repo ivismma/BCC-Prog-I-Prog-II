41576317719f5fe72abf6bced2ceda287027c392
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2023-12-06 14:45:38 -0300

    Update Questão 2.c
    Correção (fopen)

--- Listas/Lista 4 (Ponteiros e Arquivos)/Questão 2 e 3/Questão 2.c
+++ Listas/Lista 4 (Ponteiros e Arquivos)/Questão 2 e 3/Questão 2.c
@@ -1,21 +1,21 @@
 /*
-Quest„o 2) Escreva um programa que receba o nome de 2 arquivos: um bin·rio e o outro de texto, e
-ent„o
+Quest√£o 2) Escreva um programa que receba o nome de 2 arquivos: um bin√°rio e o outro de texto, e
+ent√£o
 a) leia 10 racionais do teclado;
 b) armazene em ambos os arquivos os valores que foram lidos.
 */
 
-#define TAM 30 // Tamanho m·ximo do nome do arquivo: 26 caracteres
+#define TAM 30 // Tamanho m√°ximo do nome do arquivo: 26 caracteres
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(){
-	char str[TAM], str2[TAM]; // str = texto | str2 = bin·rio
+	char str[TAM], str2[TAM]; // str = texto | str2 = bin√°rio
 	FILE *arquivo;
 	float n[10]; // Racionais
 	
-	// Armazenamento dos n˙meros racionais:
+	// Armazenamento dos n√∫meros racionais:
 	printf("Digite 10 numeros racionais (separados por espaco):\n");
 	for(int i = 0; i < 10; ++i) scanf("%f", &n[i]); getchar();
 	// Nome do arquivo de texto:
@@ -23,7 +23,6 @@ int main(){
 	scanf("%[^\n]", str); strcat(str, ".txt"); getchar();
 	
 	// Escrita em texto:
-	arquivo = fopen(str, "w");
 	if(!(arquivo = fopen(str, "w"))){
 		printf("Erro na criacao/abertura do arquivo.");
 		return -1;
@@ -32,7 +31,7 @@ int main(){
 	printf("Dados salvos (%s)", str);
 	fclose(arquivo);
 	
-	// Escrita bin·ria:
+	// Escrita bin√°ria:
 	printf("\nAgora digite o nome do arquivo binario: ");
 	scanf("%[^\n]", str2); strcat(str2, ".dat");
 	
