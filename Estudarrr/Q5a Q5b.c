742171cba7d23770283948c7c5aad8a399cf3405
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2023-12-23 05:31:30 -0300

    Update Q5a Q5b.c
    Add busca binária

--- Estudarrr/Q5a Q5b.c
+++ Estudarrr/Q5a Q5b.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-// Quest„o muito interessante e bem completa. (5a 5b - P2 antiga 2023.1.pdf)
+// Quest√£o muito interessante e bem completa. (5a 5b - P2 antiga 2023.1.pdf)
 
-// Fazendo quest„o 5 de Prova antiga porÈm c/ ponteiros.
-/*  AnotaÁıes:
-- Uma carta vale, em pontos, exatamente o seu n˙mero se o seu naipe n„o for copas.
-- Cartas de copas valem duas vezes o seu n˙mero.
-- N„o pode ser inserida na mesa carta cujo a pontuaÁ„o j· foi pontuada por outro jogador.
+// Fazendo quest√£o 5 de Prova antiga por√©m c/ ponteiros.
+/*  Anota√ß√µes:
+- Uma carta vale, em pontos, exatamente o seu n√∫mero se o seu naipe n√£o for copas.
+- Cartas de copas valem duas vezes o seu n√∫mero.
+- N√£o pode ser inserida na mesa carta cujo a pontua√ß√£o j√° foi pontuada por outro jogador.
 */
 
-// Defesas para inserÁ„o de valores n„o criada por exercÌcio ser longo, inserir valores v·lidos.
+// Defesas para inser√ß√£o de valores n√£o criada por exerc√≠cio ser longo, inserir valores v√°lidos.
 
 typedef struct {
 	char naipe; //'o','e','c' ou 'p'(ouros, espadas, copas ou paus, respec.).
-	int numero; //N˙mero da carta, entre 2 e 10.
+	int numero; //N√∫mero da carta, entre 2 e 10.
 	char jogador[100]; //Nome do jogador que a revelou;
 } Carta;
 
@@ -25,7 +25,7 @@ void exibeVetor(Carta* carta, int tam);
 int cartaPontos(Carta* carta);
 bool checaCartas(Carta* carta, int tam, int pontos);
 // bool addVetor(int *v, int tam, Carta carta);
-// possivel(jaReveladas[], int t) <-- FunÁ„o que o item b) pede.
+// possivel(jaReveladas[], int t) <-- Fun√ß√£o que o item b) pede.
 
 int main(void){
 	int n;
@@ -34,11 +34,11 @@ int main(void){
 	Carta* reveladas = (Carta*) calloc(n, sizeof(Carta));
 	// Leitura dos jogadores
 	for(int i = 0; i < n;++i){
-		bool ok; // FLAG - Se pontuaÁ„o inserida for repetida, ser· falsa e continuar· loop.
+		bool ok; // FLAG - Se pontua√ß√£o inserida for repetida, ser√° falsa e continuar√° loop.
 		printf("Jogador %d:\n", i+1);
 		printf("Nome: "); scanf("%99[^\n]", reveladas[i].jogador);
 		getchar();
-		do{ // Enquanto inserir pontuaÁ„o j· pontuada antes, inserir outra carta.
+		do{ // Enquanto inserir pontua√ß√£o j√° pontuada antes, inserir outra carta.
 			ok = true;
 			printf("Naipe: "); scanf(" %c", &reveladas[i].naipe);
 			printf("Numero: "); scanf("%d", &reveladas[i].numero);
@@ -51,7 +51,7 @@ int main(void){
 		} while(!ok);
 		printf("---------------------\n");
 	}
-	// OrdenaÁ„o dos jogadores em ordem de pontuaÁ„o e vencedor.
+	// Ordena√ß√£o dos jogadores em ordem de pontua√ß√£o e vencedor.
 	ordenaStruct(reveladas, n);
 	statusRodada(reveladas, n);
 	
@@ -59,13 +59,13 @@ int main(void){
 	return 0;
 }
 
-int cartaPontos(Carta* carta){ // Retorna quantos pontos o jogador fez com naipe e n™ de sua carta.
+int cartaPontos(Carta* carta){ // Retorna quantos pontos o jogador fez com naipe e n¬™ de sua carta.
 	if(carta->naipe == 'c') // Copas = 2x Pontos
 		return 2*carta->numero;
 	return carta->numero;
 }
 
-bool checaCartas(Carta* p, int tam, int pontos){ // Parecido com a funÁ„o q item (b) pede.
+bool checaCartas(Carta* p, int tam, int pontos){ // Parecido com a fun√ß√£o q item (b) pede.
 	for(int i = 0; i < tam;++i)
 		if(cartaPontos(&p[i]) == pontos) return false;
 	return true;
@@ -78,18 +78,18 @@ void statusRodada(Carta* reveladas, int N){ // Mostra resultado final da rodada 
 	for(int i = 1; i < N;++i) // Loop p/ descobrir maior carta e quem a possui.
 		if(cartaPontos(&reveladas[i]) > maior){
 			maior = cartaPontos(&reveladas[i]);
-			i_pessoa = i; // Armazena indice da pessoa que possui maior pontuaÁ„o.
+			i_pessoa = i; // Armazena indice da pessoa que possui maior pontua√ß√£o.
 		}
-	for(int i = N-1; i >= 0;--i){ // ExibiÁ„o dos jogadores em ordem decrescente de pontuaÁ„o
+	for(int i = N-1; i >= 0;--i){ // Exibi√ß√£o dos jogadores em ordem decrescente de pontua√ß√£o
 		int pts = cartaPontos(&reveladas[i]);
 		printf("Jogador %s: %d pontos.\n", reveladas[i].jogador, pts);
 	}
-	for(int i = 0; i < N;++i) // ExibiÁ„o do vencedor e quanto pontuou.
+	for(int i = 0; i < N;++i) // Exibi√ß√£o do vencedor e quanto pontuou.
 		soma += cartaPontos(&reveladas[i]);
 	printf("O jogador %s ganhou a rodada e levou da mesa o total de %d pontos.", 
 	reveladas[i_pessoa].jogador, soma);
 }
-// OrdenaÁ„o cresc. de struct com base na pontuaÁ„o das cartas dos jogadores.
+// Ordena√ß√£o cresc. de struct com base na pontua√ß√£o das cartas dos jogadores.
 void ordenaStruct(Carta* p, int tam){
 	bool ordenado = false;
 	int fim = tam-1;
@@ -106,20 +106,27 @@ void ordenaStruct(Carta* p, int tam){
 		--fim;
 	}
 }
-// FunÁ„o do item (b) Obs: N„o utilizei.
+// Fun√ß√£o do item (b) Obs: N√£o utilizei.
 int possivel(Carta intencao, Carta reveladas[], int t){
-	int i = 0, pontos = cartaPontos(&intencao);
-	while(i < t)  // Se encontrou pontuaÁ„o igual j· revelada, n„o È possÌvel.
-		if(cartaPontos(&reveladas[i++]) == pontos) return 0;
-	return 1;
+	int ini = 0, meio, fim = t-1;
+	pontos = cartaPontos(&intencao);
+	// Busca bin√°ria (vetor est√° crescente)
+	while (ini <= fim) { //Busca bin√°ria
+		meio = (ini + fim) / 2;
+		if (cartaPontos(reveladas[meio]) == pontos) 
+			return 0; // Carta j√° revelada.
+		if (cartaPontos(reveladas[meio]) < pontos) ini = meio + 1;
+		else fim = meio - 1;
+	}
+	return 1; // Pontua√ß√£o dispon√≠vel.
 }
 
-// Outra maneira de salvar pontuaÁıes j· reveladas (criar vetor a parte).
+// Outra maneira de salvar pontua√ß√µes j√° reveladas (criar vetor a parte).
 /*bool addVetor(int *v, int tam, Carta carta){
 	static int cont = 0;
 	for(int i = 0; i < cont;++i)
 		if(v[cont] == carta->numero)
-			return false; // PontuaÁ„o equivalente j· revelada.
+			return false; // Pontua√ß√£o equivalente j√° revelada.
 	*(v+cont++) = carta.numero;
 	return true;
 }*/
